abac4a134ffe2107a86385fc392d94877b2e958f
Eghizio <eghizio@gmail.com>
2020-06-18 02:55:16 +0200

    Added faulty page handling

--- app.js
+++ app.js
@@ -19,18 +19,27 @@ const OUTPUT_DIRECTORY = "komixxy";
     initOutputDirectory(OUTPUT_DIRECTORY);
     
     const paginate = async (page, nextPage) => {
+        const pageNumber = Number(nextPage.split("/").pop());
         await page.goto(nextPage, {waitUntil: 'networkidle2'});
-    
-        const data = await scrapeData(page);
-        data.forEach(async entry => {
-            const image_url = entry.image_src;
-            const fileName = constructFileName(entry);
-            await downloadAndSaveImage(image_url, fileName, OUTPUT_DIRECTORY);
-        });
-        console.log("\x1b[32m%s\x1b[0m", `#${nextPage.split("/").pop()} page scraped and saved!`);
 
-        nextPage = await getNextPage(page);
+        const isFaulty = await page.$("div#framework_error") ? true : false;
     
+        if(!isFaulty){
+            const data = await scrapeData(page);
+            data.forEach(async entry => {
+                const image_url = entry.image_src;
+                const fileName = constructFileName(entry);
+                await downloadAndSaveImage(image_url, fileName, OUTPUT_DIRECTORY);
+            });
+            console.log("\x1b[32m%s\x1b[0m", `#${pageNumber} page scraped and saved!`);
+
+            nextPage = await getNextPage(page);
+        }
+        else{
+            nextPage = getURL(pageNumber+1);
+            console.log("\x1b[31m%s\x1b[0m", `#${pageNumber} is faulty. Skipping to ${pageNumber+1}...`);
+        }
+        
         if(nextPage) await paginate(page, nextPage);
     };
     await paginate(page, getURL(START_PAGE));
